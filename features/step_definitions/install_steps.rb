e2f9feb225f93dd80bd6064ab4aca3bfae970b85
Nikhil Gupta <me@nikhgupta.com>
2012-12-30 01:47:01 +0530

    removed all files

--- features/step_definitions/install_steps.rb
+++
@@ -1,9 +0,0 @@
-When /install generator for this application$/ do
-  step "I run `laravel install generator --app=#{@app_path}`"
-end
-
-Then /^(?:|a )tasks? should be installed as "(.*?)"$/ do |filename|
-  filepath = @app_tester.app.tasks_file(filename)
-  @app_tester.raise_error?(File.exists?(filepath))
-  step "the stdout should contain \"Installed task\""
-end
--- lib/laravel/version.rb
+++
@@ -1,3 +0,0 @@
-module Laravel
-  VERSION = "0.5.1"
-end
